1d463483b31fb92f87472703a0f0bedfba742965
luis <joseluiscunharodrigues@gmail.com>
2026-01-03 05:29:22 +0000

    Added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1 class="text-red-300">You did it! Yes i did</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
